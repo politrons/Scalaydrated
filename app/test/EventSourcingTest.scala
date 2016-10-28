db088695b79f30ad021d74d9768881992b909154
ZH11 <Pablo.Garcia@uk.tesco.com>
2016-10-28 23:52:22 +0100

    Add test scenario

--- app/test/EventSourcingTest.scala
+++ app/test/EventSourcingTest.scala
@@ -1,8 +1,11 @@
-import persistance.CouchbaseDAO
-import src.PersistenceModel._
+import java.util.UUID
+
 import _root_.model.User
 import events.UserCreated
+import org.junit.Test
+import persistance.CouchbaseDAO
 import src.Model
+import src.PersistenceModel._
 
 /**
   * Created by pabloperezgarcia on 28/10/2016.
@@ -11,13 +14,14 @@ class EventSourcingTest {
 
   val user = initialize[User](new CouchbaseDAO())
 
+  @Test
   def create() {
-    val userName = "test"
-      val documentId: String = user.createDocument(userName)
-      val event = new UserCreated(documentId, "")
-
-      user.appendEvent(documentId, event, classOf[UserCreated], getCreateUserAction)
-      user.rehydrate(documentId)
+    val userName = UUID.randomUUID().toString
+    val documentId: String = user.createDocument(userName)
+    val event = new UserCreated(documentId, "")
+    user.appendEvent(documentId, event, classOf[UserCreated], getCreateUserAction)
+    user.rehydrate(documentId)
+    assert(user.userName.equals(userName))
   }
 
   private def getCreateUserAction: (Model, UserCreated) => Unit = {
